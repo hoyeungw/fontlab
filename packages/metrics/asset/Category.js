db8f6a7c2b8c0ea57d65b93a444e51e13cd579dd
hoyeungw <hoyeungw@outlook.com>
2022-04-24 08:00:42 +0800

    add Category

---
+++ packages/metrics/asset/Category.js
@@ -0,0 +1,8 @@
+export class Category {
+  static Upper = 'upper'
+  static Lower = 'lower'
+  static Other = 'other'
+}
+
+export const CATEGORIES = [Category.Upper, Category.Lower, Category.Other]
+
