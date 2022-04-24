52449130730797b0c59afcf6915f1543b3847016
hoyeungw <hoyeungw@outlook.com>
2022-04-24 08:01:05 +0800

    add ORTHOGRAPHY for French and Italian

--- packages/metrics/asset/DIACRITICS.js
+++ packages/metrics/asset/DIACRITICS.js
@@ -21,6 +21,32 @@ export const DIACRITICS = [
   'slash',
 ]
 
+export const ORTHOGRAPHY_FRENCH = {
+  A: ['Agrave', 'Acircumflex', 'AE'],
+  C: ['Ccedilla'],
+  E: ['Eacute', 'Egrave', 'Ecircumflex', 'Edieresis'],
+  I: ['Icircumflex', 'Idieresis'],
+  O: ['Ocircumflex', 'OE'],
+  U: ['Ugrave', 'Ucircumflex', 'Udieresis'],
+  Y: ['Ydieresis'],
+
+  a: ['agrave', 'acircumflex', 'ae'],
+  c: ['ccedilla'],
+  e: ['eacute', 'egrave', 'ecircumflex', 'edieresis'],
+  i: ['icircumflex', 'idieresis'],
+  o: ['ocircumflex', 'oe'],
+  u: ['ugrave', 'ucircumflex', 'udieresis'],
+  y: ['ydieresis']
+}
+
+export const ORTHOGRAPHY_ITALIAN = {
+  a: ['agrave'],
+  e: ['eacute', 'egrave',],
+  i: ['iacute', 'igrave', 'icircumflex'],
+  o: ['oacute', 'ograve'],
+  u: ['uacute', 'ugrave']
+}
+
 // ae
 // oe
 // ij
