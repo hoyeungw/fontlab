682b13d63324bb79109641c98d2e65e9ccf877b1
hoyeungw <hoyeungw@outlook.com>
2022-04-27 20:30:56 +0800

    improve groupsToSurject

--- packages/metrics/src/convert/groupsToSurject.js
+++ packages/metrics/src/convert/groupsToSurject.js
@@ -1,11 +1,18 @@
-import { Side } from '../../asset/Side'
+import { mutate }             from '@vect/vector-mapper'
+import { is1st, is2nd, Side } from '../../asset/Side'
+import { Group }              from '../Group'
 
 export const groupsToSurject = (groups, side) => {
-  if (side === Side.Verso) groups = groups.filter((group) => group['1st'])
-  if (side === Side.Recto) groups = groups.filter((group) => group['2nd'])
-  const surject = { '-': '-' }
-  for (let { name, names } of groups)
+  if (!groups?.length) return {}
+  if (!(groups[0] instanceof Group)) mutate(groups, Group.build)
+  if (side === Side.Verso) groups = groups.filter(is1st)
+  if (side === Side.Recto) groups = groups.filter(is2nd)
+  // groups  |> deco  |> logger
+  const surject = {}
+  for (let { side, name, names } of groups) {
+    // `[side] (${side}) [name] (${name}) [names] (${names})`  |> console.log
     for (let glyph of names)
       surject[glyph] = name
+  }
   return surject
 }
