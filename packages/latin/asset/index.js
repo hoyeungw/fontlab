c9c052c89493d21dd96f9b52091b06e8e406a38c
hoyeungw <hoyeungw@outlook.com>
2022-04-29 09:50:38 +0800

    expand ALPHABET

--- packages/latin/asset/index.js
+++ packages/latin/asset/index.js
@@ -1,8 +1,17 @@
-export { ALPHABET_LOWER }     from './ALPHABET_LOWER'
-export { ALPHABET_UPPER }     from './ALPHABET_UPPER'
-export { DIACRITICS }         from './DIACRITICS'
-export { GROUP_SCHEME_UPPER } from './GROUP_SCHEME_UPPER'
-export { GROUP_SCHEME_LOWER } from './GROUP_SCHEME_LOWER'
-export { ORTHO_FRENCH }       from './ORTHO_FRENCH'
-export { ORTHO_ITALIAN }      from './ORTHO_ITALIAN'
-export { Scope, scopeName }   from './Scope'
+import { merge }              from '@vect/vector-merge'
+import { GROUP_SCHEME_LOWER } from './GROUP_SCHEME_LOWER'
+import { GROUP_SCHEME_UPPER } from './GROUP_SCHEME_UPPER'
+
+export { DIACRITICS }               from './DIACRITICS'
+export { ORTHO_FRENCH }             from './ORTHO_FRENCH'
+export { ORTHO_ITALIAN }            from './ORTHO_ITALIAN'
+export { Scope, scopeName, SCOPES } from './Scope'
+
+export { GROUP_SCHEME_UPPER }
+export { GROUP_SCHEME_LOWER }
+export const ALPHABET_LOWER = Object.keys(GROUP_SCHEME_LOWER)
+export const ALPHABET_UPPER = Object.keys(GROUP_SCHEME_UPPER)
+export const ALPHABET = merge(ALPHABET_UPPER, ALPHABET_LOWER)
+
+// export { ALPHABET_LOWER }     from './ALPHABET_LOWER'
+// export { ALPHABET_UPPER }     from './ALPHABET_UPPER'
