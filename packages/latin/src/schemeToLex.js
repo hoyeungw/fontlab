c8502052c7c11719ec050d14ec7696ce499ade20
hoyeungw <hoyeungw@outlook.com>
2022-04-29 09:51:19 +0800

    remove schemeToLex

--- packages/latin/src/schemeToLex.js
+++
@@ -1,9 +0,0 @@
-export const schemeToLex = scheme => {
-  const o = {}
-  for (let y in scheme) {
-    if (Array.isArray(scheme[y])) for (let x of scheme[y]) {
-      if (!(x in o)) o[x] = y
-    }
-  }
-  return o
-}
