a5ccd20102fd183f2950744e18f6eca8a54fe6e7
hoyeungw <hoyeungw@outlook.com>
2022-04-29 09:50:44 +0800

    add SCOPES

--- packages/latin/asset/Scope.js
+++ packages/latin/asset/Scope.js
@@ -6,6 +6,8 @@ export class Scope {
   static get Letter() { return Scope.Lower | Scope.Upper }
 }
 
+export const SCOPES = [ Scope.Upper, Scope.Lower, Scope.Other ]
+
 export const scopeName = scope => {
   for (let k in Scope) {
     if (Scope[k] === scope) return k
