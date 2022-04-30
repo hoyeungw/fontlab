d6b6ffec7755e319b2bffdd0735b445d82ebda89
hoyeungw <hoyeungw@outlook.com>
2022-04-30 17:41:49 +0800

    simplify groupsToSurject

--- packages/master/utils/groupsToSurject.js
+++ packages/master/utils/groupsToSurject.js
@@ -1,15 +1,7 @@
-import { mutate }             from '@vect/vector-mapper'
-import { is1st, is2nd, Side } from '../asset/Side'
-
-export const groupsToSurject = (groups, side) => {
+export const groupsToSurject = (groups) => {
   if (!groups?.length) return {}
-  if (!(groups[0] instanceof Group)) mutate(groups, Group.build)
-  if (side === Side.Verso) groups = groups.filter(is1st)
-  if (side === Side.Recto) groups = groups.filter(is2nd)
-  // groups  |> deco  |> logger
   const surject = {}
-  for (let { side, name, names } of groups) {
-    // `[side] (${side}) [name] (${name}) [names] (${names})`  |> console.log
+  for (let { name, names } of groups) {
     for (let glyph of names)
       surject[glyph] = name
   }
