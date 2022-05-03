5fcfe99438c589194963ea3fcbc445857bbf803c
hoyeungw <hoyeungw@outlook.com>
2022-05-03 14:34:13 +0800

    renamed makeGlyphToRegroup to glyphsToSurject

--- packages/master/src/Grouped.js
+++ packages/master/src/Grouped.js
@@ -1,7 +1,7 @@
 import { groupedToSurject }       from '@analys/convert'
 import { Latin, stringAscending } from '@fontlab/latin'
 import { valid }                  from '@typen/nullish'
-import { mapKeyValue }            from '@vect/object-mapper'
+import { vectorToObject }         from '@vect/object-init'
 import { filterByValue }          from '@vect/object-select'
 import { SideUtil }               from '../asset'
 import { Group }                  from './Group'
@@ -11,13 +11,10 @@ export class Grouped {
     samples.sort((a, b) => a.side === b.side ? stringAscending(a.name, b.name) : a.side - b.side)
     return Object.fromEntries(samples.map((group) => [ group.name, Group.build(group) ]))
   }
-  static makeGlyphToRegroup(grouped, regroupScheme) {
-    const glyphToPrevGroup = grouped|> groupedToSurject
-    const letterToNextGroup = regroupScheme |> groupedToSurject
-    // glyphToPrevGroup |> deco |> says['Grouped'].br('glyphToPrevGroup')
-    // letterToNextGroup |> deco |> says['Grouped'].br('letterToNextGroup')
-    const toGroup = letter => valid(letterToNextGroup[letter]) ? ('@' + letterToNextGroup[letter]) : letter
-    return mapKeyValue(glyphToPrevGroup, glyph => glyph|> Latin.letter|> toGroup)
-  }
   static select(grouped, side) { return filterByValue(grouped, SideUtil.filterFactory(side)) }
+  static glyphsToSurject(glyphs, letterGrouped) {
+    const letterToNextGroup = letterGrouped|> groupedToSurject
+    const toGroup = letter => valid(letterToNextGroup[letter]) ? ('@' + letterToNextGroup[letter]) : letter
+    return vectorToObject(glyphs, glyph => glyph|> Latin.letter|> toGroup)
+  }
 }
