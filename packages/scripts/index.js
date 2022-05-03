2e5586f23815193697dc04e4f561f25523f82446
hoyeungw <hoyeungw@outlook.com>
2022-05-03 20:34:37 +0800

    add @fontlab/scripts

--- packages/scripts/index.js
+++ packages/scripts/index.js
@@ -1,21 +1,3 @@
-export {
-  ALPHABET_LOWER,
-  ALPHABET_UPPER,
-  ALPHABET,
-  DIACRITICS,
-  ORTHO_FRENCH,
-  ORTHO_ITALIAN,
-  GROUP_SCHEME_LOWER,
-  GROUP_SCHEME_UPPER,
-  Scope,
-  scopeName,
-  SCOPES
-} from './asset/index'
-
-export { Latin } from './src/Latin'
-
-export {
-  shortenWeight,
-  stringAscending,
-  stringValue,
-} from './util/index'
+export { PhenoGroupsIO }  from './src/PhenoGroupsIO'
+export { PhenoPairsIO }   from './src/PhenoPairsIO'
+export { PhenoMetricsIO } from './src/PhenoMetricsIO'
