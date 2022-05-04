c3b1870afbd2d7047fed1458c11de0e6a242f39f
hoyeungw <hoyeungw@outlook.com>
2022-05-04 17:55:35 +0800

    improve performance

--- packages/pheno/src/Pheno.js
+++ packages/pheno/src/Pheno.js
@@ -1,21 +1,19 @@
-import { CrosTab }                                               from '@analys/crostab'
-import { filterIndexed as filterIndexedCrostab }                 from '@analys/crostab-indexed'
-import { almostEqual, round }                                    from '@aryth/math'
-import { ALPHABET_UPPER, Latin, shortenWeight, stringAscending } from '@fontlab/latin'
-import { Master }                                                from '@fontlab/master'
-import { Metric }                                                from '@fontlab/metric'
-import { ros, says }                                             from '@spare/logger'
-import { cr }                                                    from '@spare/xr'
-import { camelToSnake }                                          from '@texting/phrasing'
-import { nullish }                                               from '@typen/nullish'
-import { filterIndexed, transpose, updateCell }                  from '@vect/nested'
-import { iterateValues, mapKeyValue, mapValues }                 from '@vect/object-mapper'
-import { getFace, LAYER }                                        from '../asset'
+import { CrosTab }                                   from '@analys/crostab'
+import { almostEqual, round }                        from '@aryth/math'
+import { ALPHABET_UPPER, asc, Latin, shortenWeight } from '@fontlab/latin'
+import { Master }                                    from '@fontlab/master'
+import { Metric }                                    from '@fontlab/metric'
+import { $, says }                                   from '@spare/xr'
+import { valid }                                     from '@typen/nullish'
+import { filterIndexed, transpose, updateCell }      from '@vect/nested'
+import { mapKeyValue, mapValues, mutate }            from '@vect/object-mapper'
+import { appendValue }                               from '@vect/object-update'
+import { getFace, LAYER }                            from '../asset'
 
+// noinspection CommaExpressionJS
 export class Pheno {
   dataType = 'com.fontlab.metrics'
   /** @type {Object<string,Master>}                */ layerToMaster // LayersToKerning
-  /** @type {Object<string,Object<string,Metric>>} */ glyphLayerToMetric // GlyphsToLayersToMetrics
   /** @type {Object<string,Object<string,Metric>>} */ layerToMetrics // GlyphsToLayersToMetrics
   upm
 
@@ -37,21 +35,17 @@ export class Pheno {
 
   get layers() { return Object.keys(this.layerToMaster) }
   get face() { return getFace(this.layerToMaster) }
-  get glyphLayerToMetric() { return this.layerToMetrics |> transpose }
+  get glyphLayerToMetric() { return this.layerToMetrics|> transpose }
 
   /** @return {Master} */
   master(layer) { return this.layerToMaster[layer ?? this.face] }
-  metricSet(layer) { return this.layerToMetrics[layer ?? this.face] }
+  metrics(layer) { return this.layerToMetrics[layer ?? this.face] }
+  glyphs(layer) { return Object.keys(this.metrics(layer ?? this.face)).sort(asc) }
 
-  alphabetGroups() {
-    const grouped = {}
-    const glyphs = Object.keys(this.glyphLayerToMetric).sort(stringAscending)
-    for (let glyph of glyphs) {
-      const letter = Latin.letterOrNull(glyph)
-      if (!letter) continue;
-      (grouped[letter] ?? (grouped[letter] = [])).push(glyph)
-    }
-    iterateValues(grouped, list => list.sort(stringAscending))
+  alphabetGrouped(layer) {
+    let grouped = {}, x
+    for (let g of this.glyphs(layer)) if ((x = Latin.letterOrNull(g))) appendValue.call(grouped, x, g)
+    for (let x of grouped) grouped[x].sort(asc)
     return grouped
   }
   sidebearingTable(alphabet = ALPHABET_UPPER) {
@@ -68,25 +62,19 @@ export class Pheno {
     return crostab.mutateBanner(shortenWeight)
   }
 
-  regroupMasters(regroupScheme) {
-    for (let layer of this.layers)
-      this.layerToMaster[layer] = this.layerToMaster[layer].regroup(regroupScheme)
-    return this
-  }
-  updatePairsByCrostab(crostab) {
-    if (!crostab) return this
-    for (let layer of this.layers) {
-      const pairs = this.layerToMaster[layer].pairs
+  mutateRegroup(regroupScheme) { return mutate(this.layerToMaster, m => m.regroup(regroupScheme)), this }
+  mutatePairs(nextPairs) {
+    function zero(v) { return almostEqual(v, 0, 0.1) }
+    function val(x, y) { return (this[x] ?? {})[y] }
+    for (let [ layer, { pairs } ] of this.layerToMaster) {
       let num = 0
-      for (let [ x, y, val ] of filterIndexedCrostab(crostab, (x, y, v) => !almostEqual(v, 0, 0.1))) {
-        const raw = (pairs[x] ?? {})[y] ?? null
-        if (nullish(val) || almostEqual(val, 0, 0.1) || raw === val) continue
+      for (let [ x, y, v ] of filterIndexed(nextPairs,
+        (nx, ny, nv) => valid(nv) && !zero(nv) && val.call(pairs, nx, ny) !== nv)) {
         num++
-        if (layer === 'Regular') `layer ( ${ros(layer)} ) cell( ${x}, ${y} ) = (${raw}) -> (${val})` |> says['Profile'].br(ros(camelToSnake('updatePairsByCrostab')))
-        updateCell.call(pairs, x, y, val)
+        // if (layer === 'Regular') `layer ( ${ros(layer)} ) cell( ${x}, ${y} ) = (${raw}) -> (${v})` |> says['Pheno'].br('mutatePairs')
+        updateCell.call(pairs, x, y, v)
       }
-      cr('updatePairsByCrostab')[LAYER](ros(layer))['updatedPairs'](num) |> says['Profile']
-      // `layer ( ${ros(layer)} ) updated (${num}) pairs` |> says['Pheno'].br(ros(camelToSnake('updatePairsByCrostab')))
+      $[LAYER](layer)['updated'](num) |> says['Pheno'].br('mutatePairs')
     }
     return this
   }
