552549bc91988ad148e0996d2c96b13a66f460b0
hoyeungw <hoyeungw@outlook.com>
2022-05-04 09:56:45 +0800

    add PhenoGroupsIO.mutateRegroup

--- packages/scripts/src/PhenoGroupsIO.js
+++ packages/scripts/src/PhenoGroupsIO.js
@@ -1,3 +1,29 @@
-export class PhenoGroupsIO {
+import { parsePath }                          from '@acq/path'
+import { decoKerningClasses, GROUPS_CHALENE } from '@fontlab/master'
+import { FONTLAB }                            from '@fontlab/pheno'
+import { deco }                               from '@spare/logger'
+import { says }                               from '@spare/xr'
+import { PhenoIO }                            from './PhenoIO'
 
+const PHENO_GROUPS_IO = 'PhenoGroupsIO'
+const REGULAR = 'Regular'
+
+export class PhenoGroupsIO {
+  static async mutateRegroup(srcVfm, dest, layer = REGULAR, regroups = GROUPS_CHALENE) {
+    const pheno = await PhenoIO.readPheno(srcVfm)
+    const nextMaster = pheno.master(layer).regroup(regroups)
+    nextMaster.kerningClasses |> decoKerningClasses |> says[FONTLAB].br(PHENO_GROUPS_IO).br('mutateRegroup').br('pairs')
+    nextMaster.pairs |> deco |> says[FONTLAB].br(PHENO_GROUPS_IO).br('mutateRegroup').br('pairs')
+
+    for (let currLayer in pheno.layerToMaster) {
+      pheno.layerToMaster[currLayer] = nextMaster
+    }
+
+    const { base, ext } = parsePath(srcVfm)
+    await PhenoIO.savePheno(
+      pheno,
+      dest + '/' + base + ext,
+      { groups: true, pairs: true, metrics: true, suffix: '-regrouped' }
+    )
+  }
 }
