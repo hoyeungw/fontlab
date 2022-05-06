bf19aa55dfd7cdb59906dd84cf2269e4f907cb01
hoyeungw <hoyeungw@outlook.com>
2022-05-06 12:56:11 +0800

    rename properties

--- packages/metric/src/Metric.js
+++ packages/metric/src/Metric.js
@@ -1,47 +1,52 @@
-import { round }    from '@aryth/math'
-import { NUM, STR } from '@typen/enum-data-types'
+import { round }        from '@aryth/math'
+import { NUM, STR }     from '@typen/enum-data-types'
+import { parseNum }     from '@typen/num-strict'
+import { calc }         from './calc'
+import { metricToJson } from './json'
+
 
 export class Metric {
   lsb
   rsb
-  metricsLeft
-  metricsRight
+  lpt
+  rpt
   width
-  xmax
-  xmin
-  ymax
-  ymin
+  xhi
+  xlo
+  yhi
+  ylo
   constructor(metrics) {
     this.lsb = metrics.lsb
     this.rsb = metrics.rsb
-    this.metricsLeft = metrics.metricsLeft
-    this.metricsRight = metrics.metricsRight
+    this.lpt = metrics.metricsLeft
+    this.rpt = metrics.metricsRight
     this.width = metrics.width
-    this.xmax = metrics.xmax
-    this.xmin = metrics.xmin
-    this.ymax = metrics.ymax
-    this.ymin = metrics.ymin
+    this.xhi = metrics.xmax
+    this.xlo = metrics.xmin
+    this.yhi = metrics.ymax
+    this.ylo = metrics.ymin
   }
   static build(metrics) { return new Metric(metrics) }
-  get relLSB() { return this.metricsLeft ?? round(this.lsb) }
-  get relRSB() { return this.metricsRight ?? round(this.rsb) }
-  set relLSB(v) {
-    if (typeof v === NUM) { return this.lsb = v }
-    if (typeof v === STR) {
-      let n = parseFloat(v.replace(/^=/g, ''))
-      return isNaN(v - n) ? (this.metricsLeft = v) : (this.lsb = n)
+
+  get relLSB() { return this.lpt?.length ? this.lpt : round(this.lsb) }
+  get relRSB() { return this.rpt?.length ? this.rpt : round(this.rsb) }
+  set sb(v) { this.lsb = v, this.rsb = v }
+  set pt(v) { this.lpt = v, this.rpt = v }
+  update(side, value, cfg) {
+    const label = (side & 1) ? 'l' : (side & 2) ? 'r' : ''
+    const SB = label + 'sb', PT = label + 'pt'
+    if (typeof value === NUM) { return this[SB] = value }
+    if (typeof value === STR && (value = value.trim()).length && /\w/.test(value)) {
+      const [ eq, exp ] = /^=/g.test(value) ? [ '=', value.replace(/=/g, '') ] : [ null, value ]
+      const tbd = /[a-z\+\-\*\/]/gi.test(exp)
+      const val = tbd && cfg ? calc.call(cfg, exp) : parseNum(exp)
+      if (!isNaN(val)) { this[SB] = round(parseNum(val)) }
+      if (eq || tbd) { this[PT] = value }
     }
+    return this
   }
-  set relRSB(v) {
-    if (typeof v === NUM) { return this.rsb = v }
-    if (typeof v === STR) {
-      let n = parseFloat(v.replace(/^=/g, ''))
-      return isNaN(v - n) ? (this.metricsRight = v) : (this.rsb = n)
-    }
-  }
-  relSB() { return { lsb: this.relLSB, rsb: this.relRSB } }
-  absSB() { return { lsb: this.lsb, rsb: this.rsb } }
+  toJson() { return metricToJson(this) }
   toString() {
-    return `[sb] (${this.lsb},${this.rsb}) [wd] (${this.width}) [bound] ({ xmin:${this.xmin},xmax:${this.xmax},ymin:${this.ymin},ymax:${this.ymax})`
+    return `[sb] (${this.lsb},${this.rsb}) [wd] (${this.width}) [x] (${this.xlo},${this.xhi}) [y] (${this.ylo},${this.yhi})`
   }
 }
