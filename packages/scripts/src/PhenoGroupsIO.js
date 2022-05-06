2f550f2ebb31b4656849be9d689fa6f4a5a17bed
hoyeungw <hoyeungw@outlook.com>
2022-05-06 20:56:26 +0800

    add exportRegroupScheme

--- packages/scripts/src/PhenoGroupsIO.js
+++ packages/scripts/src/PhenoGroupsIO.js
@@ -1,22 +1,53 @@
-import { parsePath }          from '@acq/path'
-import { FONTLAB }            from '@fontlab/pheno'
-import { deco }               from '@spare/logger'
-import { says }               from '@spare/xr'
-import { GROUPS_CHALENE }     from '../../../resources/schemes/GROUPS_CHALENE'
-import { decoKerningClasses } from '../utils/decoKerningClasses'
-import { PhenoIO }            from './PhenoIO'
+import { parsePath }                   from '@acq/path'
+import { groupedToSurject }            from '@analys/convert'
+import { UNION }                       from '@analys/enum-join-modes'
+import { tableCollectionToWorkbook }   from '@analys/excel'
+import { Table }                       from '@analys/table'
+import { merge }                       from '@analys/table-join'
+import { Scope }                       from '@fontlab/enum-scope'
+import { Side, sideName }              from '@fontlab/enum-side'
+import { Grouped }                     from '@fontlab/kerning-class'
+import { asc, Latin }                  from '@fontlab/latin'
+import { glyphTrimBeta }               from '@fontlab/master'
+import { FONTLAB, GLYPH }              from '@fontlab/pheno'
+import { deco, decoString, decoTable } from '@spare/logger'
+import { says }                        from '@spare/xr'
+import { indexed }                     from '@vect/object-mapper'
+import xlsx                            from 'xlsx'
+import { GROUPS_CHALENE }              from '../../../resources/schemes/GROUPS_CHALENE'
+import { decoKerningClasses }          from '../utils/decoKerningClasses'
+import { PhenoIO }                     from './PhenoIO'
 
 const CLASS = 'PhenoGroupsIO'
-const REGULAR = 'Regular'
+const LAYER = 'Regular'
 
 export class PhenoGroupsIO {
-  static async exportRegrouped(srcVfm, dest, regroups = GROUPS_CHALENE) {
-    const { base, ext } = parsePath(srcVfm)
+  static async exportRegrouped(srcVfm, destVfm, regroups = GROUPS_CHALENE, logLayer = LAYER) {
     const pheno = await PhenoIO.readPheno(srcVfm)
     pheno.mutateGroups(regroups)
-    pheno.master().kerningClasses |> decoKerningClasses |> says[FONTLAB].br(CLASS).br('exportRegrouped').br('pairs')
-    pheno.master().pairs |> deco |> says[FONTLAB].br(CLASS).br('exportRegrouped').br('pairs')
-    await PhenoIO.savePheno(pheno, dest + '/' + base + '-regrouped' + ext, { groups: true, pairs: true, metrics: true })
-    await PhenoIO.saveClasses(pheno, dest + '/' + base + '-regrouped' + '.json',)
+    pheno.master(logLayer).kerningClasses |> decoKerningClasses |> says[FONTLAB].br(CLASS).br('exportRegrouped').kerningClasses(logLayer)
+    pheno.master(logLayer).pairs |> deco |> says[FONTLAB].br(CLASS).br('exportRegrouped').pairs(logLayer)
+    await PhenoIO.savePheno(pheno, destVfm, { groups: true, pairs: true, metrics: true })
+    const { dir, base } = parsePath(destVfm)
+    await PhenoIO.saveClasses(pheno, dir + '/' + base + '.json',)
+  }
+  static async exportRegroupsScheme(groups, destXlsx) {
+    const
+      { Verso, Recto } = Side,
+      { Lower, Upper } = Scope,
+      JOIN_SPEC = { fields: [ GLYPH ], joinType: UNION, fillEmpty: '' }
+    function groupsToTable(groups, side, scope) {
+      const scopeFilter = Latin.filterFactory(scope)
+      const surjectV = Grouped.from(groups.filter(({ name }) => scopeFilter(glyphTrimBeta(name))), side)|> groupedToSurject
+      return Table.from({ title: side = sideName(side), head: [ GLYPH, side ], rows: [ ...indexed(surjectV) ] })
+    }
+    const tableCollection = {
+      upper: Table.from(merge.call(JOIN_SPEC, groupsToTable(groups, Verso, Upper), groupsToTable(groups, Recto, Upper))).sort(GLYPH, asc),
+      lower: Table.from(merge.call(JOIN_SPEC, groupsToTable(groups, Verso, Lower), groupsToTable(groups, Recto, Lower))).sort(GLYPH, asc)
+    }
+    tableCollection.upper |> decoTable |> says[FONTLAB].br(CLASS).br('exportRegroupsScheme').br('upper')
+    tableCollection.lower |> decoTable |> says[FONTLAB].br(CLASS).br('exportRegroupsScheme').br('lower')
+    xlsx.writeFile(tableCollectionToWorkbook(tableCollection), destXlsx);
+    `[dest] (${destXlsx |> decoString})` |> says[FONTLAB].br(CLASS).br('exportRegroupsScheme')
   }
 }
