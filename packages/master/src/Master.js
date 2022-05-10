b1837b64bc8b43108ae31ec7cec314ea0ed35909
hoyeungw <hoyeungw@outlook.com>
2022-05-10 20:20:17 +0800

    improve performance

--- packages/master/src/Master.js
+++ packages/master/src/Master.js
@@ -1,13 +1,16 @@
-import { groupedToSurject, nestedToCrostab }                 from '@analys/convert'
-import { MIN }                                               from '@analys/enum-pivot-mode'
-import { Side }                                              from '@fontlab/enum-side'
-import { Grouped }                                           from '@fontlab/kerning-class'
-import { Latin }                                             from '@fontlab/latin'
-import { parseNum }                                          from '@typen/numeral'
-import { head as yKeys, indexed, side as xKeys, updateCell } from '@vect/nested'
-import { CONVERT_OPTIONS }                                   from '../asset'
-import { AT, getGlyph, glyphTrim, offAT, unionAcquire }      from '../utils'
-import { masterToJson }                                      from './masterToJson'
+import { groupedToSurject, nestedToCrostab, surjectToGrouped } from '@analys/convert'
+import { MIN }                                                 from '@analys/enum-pivot-mode'
+import { CONVERT_OPTIONS }                                     from '@fontlab/constants'
+import { Side }                                                from '@fontlab/enum-side'
+import { Group, Grouped, LetterGrouped }                       from '@fontlab/kerning-class'
+import { AT, getGlyph, glyphTrim, offAT, Pairs }               from '@fontlab/kerning-pairs'
+import { Latin }                                               from '@fontlab/latin'
+import { parseNum }                                            from '@typen/numeral'
+import { ob }                                                  from '@vect/object-init'
+import { indexed }                                             from '@vect/object-mapper'
+import { union }                                               from '@vect/vector-algebra'
+import { Stat }                                                from '@vect/vector-stat'
+import { masterToJson }                                        from './masterToJson'
 
 export class Master {
   /** @type {string}                                      */ name
@@ -24,58 +27,50 @@ export class Master {
 
   get kerningClasses() { return Object.values(this.grouped) }
 
-  glyphsFromPairs(side) {
-    const list = []
-    if (side & 1) unionAcquire(list, xKeys(this.pairs))
-    if (side & 2) unionAcquire(list, yKeys(this.pairs))
-    return list
-  }
   glyphs(side) {
     let glyphs = Object.keys(groupedToSurject(Grouped.select(this.grouped, side)))
-    if (side & 1) unionAcquire(glyphs, xKeys(this.pairs).map(glyphTrim))
-    if (side & 2) unionAcquire(glyphs, yKeys(this.pairs).map(glyphTrim))
+    if (side & 1) glyphs = union(glyphs, Pairs.prototype.head.call(this.pairs).map(glyphTrim))
+    if (side & 2) glyphs = union(glyphs, Pairs.prototype.side.call(this.pairs).map(glyphTrim))
     return glyphs
   }
 
   get flattenPairs() {
-    const
-      xG = Grouped.select(this.grouped, Side.Verso),
-      yG = Grouped.select(this.grouped, Side.Recto),
-      nested = {}, fake = [], update = updateCell.bind(nested)
-    for (let [ xn, yn, kern ] of indexed(this.pairs)) {
-      if (AT.test(xn) && (xn = offAT(xn))) {
-        if (AT.test(yn) && (yn = offAT(yn)))
-          for (let x of (xG[xn] ?? fake)) for (let y of (yG[yn] ?? fake)) update(x, y, kern)
-        else
-          for (let x of (xG[xn] ?? fake)) update(x, yn, kern)
-      }
-      else {
-        if (AT.test(yn) && (yn = offAT(yn)))
-          for (let y of (yG[yn] ?? fake)) update(xn, y, kern)
-        else
-          update(xn, yn, kern)
-      }
-    }
-    return nested
+    const groupedX = Grouped.select(this.grouped, Side.Verso)
+    const groupedY = Grouped.select(this.grouped, Side.Recto)
+    return Pairs.prototype.flatten.call(this.pairs, groupedX, groupedY)
+  }
+  updatePairs(nextPairs) {
+    return Pairs.prototype.update.call(this.pairs, nextPairs)
+  }
+  regroup(regroups, xGlyphs, yGlyphs) {
+    const { Verso, Recto } = Side
+    if (!xGlyphs) xGlyphs = this.glyphs(Verso)
+    if (!yGlyphs) yGlyphs = this.glyphs(Recto)
+    const surjV = LetterGrouped.prototype.toSurject.apply(Grouped.from(regroups, Verso), xGlyphs)  // ...master.glyphs(Verso), ...pheno.glyphs(layer)
+    const surjR = LetterGrouped.prototype.toSurject.apply(Grouped.from(regroups, Recto), yGlyphs)  // ...master.glyphs(Recto), ...pheno.glyphs(layer)
+    const groupedV = surjectToGrouped(surjV)
+    const groupedR = surjectToGrouped(surjR)
+    return Master.build(
+      this.name,
+      ob(
+        ...indexed(groupedV, (k, v) => AT.test(k) && v?.length, (k, v) => [ k = offAT(k), Group.verso(k, v) ]),
+        ...indexed(groupedR, (k, v) => AT.test(k) && v?.length, (k, v) => [ k = offAT(k), Group.recto(k, v) ]),
+      ),
+      Pairs.prototype.regroup.call(this.flattenPairs, surjV, surjR, Stat.min)
+    )
   }
 
   groupCrostab(mode = MIN, scopeX, scopeY, po) {
-    const xf = Latin.factory(scopeX), yf = Latin.factory(scopeY)
-    return nestedToCrostab(
-      this.pairs, mode,
-      (x, y) => xf(getGlyph(x)) && yf(getGlyph(y)),
-      (x, y, kern) => [ x, y, parseNum(kern) ],
-      po,
-    )
+    const
+      xf = Latin.factory(scopeX), yf = Latin.factory(scopeY),
+      by = (x, y) => xf(getGlyph(x)) && yf(getGlyph(y))
+    return nestedToCrostab(this.pairs, mode, by, (x, y, kern) => [ x, y, parseNum(kern) ], po,)
   }
-  letterCrostab(mode = MIN, scopeX, scopeY, po) {
-    const xf = Latin.factory(scopeX), yf = Latin.factory(scopeY)
-    return nestedToCrostab(
-      this.flattenPairs, mode,
-      (x, y) => xf(x) && yf(y),
-      (x, y, kern) => [ x, y, parseNum(kern) ],
-      po,
-    )
+  flattenCrostab(mode = MIN, scopeX, scopeY, po) {
+    const
+      xf = Latin.factory(scopeX), yf = Latin.factory(scopeY),
+      by = (x, y) => xf(x) && yf(y)
+    return nestedToCrostab(this.flattenPairs, mode, by, (x, y, kern) => [ x, y, parseNum(kern) ], po,)
   }
 
   toJson(options = CONVERT_OPTIONS) { return masterToJson.call(options, this) }
